d542b1a050d4cbbb2dc990cb49c9c2b7f61e049a
Nicole Antonacci <nicole.antonacci@gmail.com>
2019-09-14 10:47:18 -0700

    Tests written.

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
+import Nav from './components/Nav.js';
 
-it('renders without crashing', () => {
+
+test('App renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+test('Nav renders without crashing', () => {
+  const div = document.createElement('div');
+  render(<Nav />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
